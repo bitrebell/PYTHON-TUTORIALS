d11f00a6a330cc0e4c5b60736fd10d503fb35faa
ADIL SHIEKH <114225192+Darkranger00@users.noreply.github.com>
2022-09-28 15:52:25 +0530

    New files

--- claasss.py
+++ claasss.py
@@ -11,3 +11,24 @@
 # larry.subjects = ["physics", "cemistry"]
 # print(harry.section, larry.subjects)
 
+
+class Employee:
+    no_of_leaves = 8
+
+    def printdetails(self):
+        return f"Name is {self.name}. Salaary is {self.salary} and role is {self.role}"
+
+harry = Employee()
+aadil = Employee()
+
+# n1 = input("enter employee name\n")
+
+harry.name = "Harry john"
+harry.salary = "6754"
+harry.role = "Instructor"
+
+aadil.name = "Aadil shiekh"
+aadil.salary = "7873"
+aadil.role = "CEO"
+
+print(aadil.printdetails())

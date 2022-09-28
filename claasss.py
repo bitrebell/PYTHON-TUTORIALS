abdd5e53e3076b5082cdb731d6f0344960eabc07
ADIL SHIEKH <114225192+Darkranger00@users.noreply.github.com>
2022-09-28 15:18:56 +0530

    new files

---
+++ claasss.py
@@ -0,0 +1,13 @@
+# class student:
+#     pass
+#
+# harry = student()
+# larry = student()
+#
+# harry.name = "HARRY"
+# harry.std = 12
+# harry.section = 1
+# larry.std = 10
+# larry.subjects = ["physics", "cemistry"]
+# print(harry.section, larry.subjects)
+
